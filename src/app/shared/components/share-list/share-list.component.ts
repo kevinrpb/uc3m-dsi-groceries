cd61c201796f5ea15a81355bf105a895fea5ff1b
vgranados <100348903@alumnos.uc3m.es>
2020-05-02 12:55:49 +0200

    Share list update

--- src/app/shared/components/share-list/share-list.component.ts
+++ src/app/shared/components/share-list/share-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { FormControl, Validators } from '@angular/forms';
 import { ListService } from 'src/app/core/lists/lists.service';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { UserGender } from '../../models/user.model';
 
 @Component({
   selector: 'app-share-list',
@@ -23,6 +25,10 @@ export class ShareListComponent implements OnInit {
 
   public addMember() {
     this.listService.addParticipant(this.data.lid, this.emailFormControl.value)
+  }
+  
+  public showDefaultProfilePic(event: any, gender: UserGender) {
+    event.target.src = `assets/images/placeholder_image_${gender ? gender : 'male'}.png`;
   }
 
 }
