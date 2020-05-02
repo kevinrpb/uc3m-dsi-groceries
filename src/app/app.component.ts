519e5b788d0288cac279ae6a43e4a620f53e6225
vgranados <100348903@alumnos.uc3m.es>
2020-05-02 02:31:21 +0200

    Android on keyboard open fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,28 @@ export class AppComponent implements OnInit {
 
   constructor(
 
-  ) {}
+  ) { }
 
+  private screenHeight: Number
   public bigScreen: boolean
 
   ngOnInit() {
-    this.bigScreen = window.screen.height > 675
+    this.screenHeight = window.innerHeight
+
+    if (/Android/.test(navigator.appVersion)) {
+      window.addEventListener("resize", (event) => {
+        console.log(window.innerHeight)
+        if (window.innerHeight == this.screenHeight) {
+          document.body.classList.remove('keyboard')
+          return
+        }
+        if (document.activeElement.tagName == "INPUT" || document.activeElement.tagName == "TEXTAREA") {
+          document.body.classList.add('keyboard')
+        }
+      })
+    }
+
+    this.bigScreen = this.screenHeight > 675
   }
 
 }
